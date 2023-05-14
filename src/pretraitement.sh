b01c84dc489edbc275be8c428ccf7fae4b5c6972
Sazuna <liza.fretel@gmail.com>
2023-05-14 12:07:12 +0200

    Généralisation de pretraitement.sh

--- src/pretraitement.sh
+++ src/pretraitement.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
+SOURCE=$1
+CIBLE=$2
+
+source moses.env
+EXT1=$(echo $SOURCE | rev | cut -d'.' -f 1 | rev)
+EXT2=$(echo $CIBLE | rev | cut -d'.' -f 1 | rev)
+
 # Tokenisation
-../../mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < ../Translation/Europarl_train_10k.en > ../Translation/Europarl_train_10k.tok.en
-../../mosesdecoder/scripts/tokenizer/tokenizer.perl -l fr < ../Translation/Europarl_train_10k.fr > ../Translation/Europarl_train_10k.tok.fr
+SOURCE1=$(echo $SOURCE | sed 's/10k\./10k.tok./g')
+CIBLE1=$(echo $CIBLE | sed 's/10k\./10k.tok./g')
+$SCRIPTS_HOME/tokenizer/tokenizer.perl -l en < $SOURCE > $SOURCE1
+$SCRIPTS_HOME/tokenizer/tokenizer.perl -l en < $CIBLE > $CIBLE1
 
 # Apprentissage du modèle de transformation
-../../mosesdecoder/scripts/recaser/train-truecaser.perl --model ../Translation/truecase-model.en --corpus ../Translation/Europarl_train_10k.tok.en
-../../mosesdecoder/scripts/recaser/train-truecaser.perl --model ../Translation/truecase-model.fr --corpus ../Translation/Europarl_train_10k.tok.fr
+MODEL1=${SOURCE%/*}/truecase-model.$EXT1
+MODEL2=${CIBLE%/*}/truecase-model.$EXT2
+$SCRIPTS_HOME/recaser/train-truecaser.perl --model $MODEL1 --corpus $SOURCE1
+$SCRIPTS_HOME/recaser/train-truecaser.perl --model $MODEL2 --corpus $CIBLE1 
+
 
 # Transformation des majuscules en minuscules
-
-../../mosesdecoder/scripts/recaser/truecase.perl --model ../Translation/truecase-model.en < ../Translation/Europarl_train_10k.tok.en > ../Translation/Europarl_train_10k.tok.true.en
-../../mosesdecoder/scripts/recaser/truecase.perl --model ../Translation/truecase-model.fr < ../Translation/Europarl_train_10k.tok.fr > ../Translation/Europarl_train_10k.tok.true.fr
+SOURCE2=$(echo $SOURCE1 | sed 's/tok\./tok.true./g')
+CIBLE2=$(echo $CIBLE1 | sed 's/tok\./tok.true./g')
+$SCRIPTS_HOME/recaser/truecase.perl --model $MODEL1 < $SOURCE1 > $SOURCE2
+$SCRIPTS_HOME/recaser/truecase.perl --model $MODEL2 < $CIBLE1 > $CIBLE2
 
 
 # Nettoyage en imitant la longueur des phrases à 80 caractères
 
-../../mosesdecoder/scripts/training/clean-corpus-n.perl ../Translation/Europarl_train_10k.tok.true fr en ../Translation/Europarl_train_10k.tok.true.clean 1 80
+BASE=${SOURCE2%.*}
+$SCRIPTS_HOME/training/clean-corpus-n.perl $BASE $EXT1 $EXT2 $BASE.clean 1 80
